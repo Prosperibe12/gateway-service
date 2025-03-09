8b91a02f0f93073f7e47302c4a7c5861001328e3
Prosper Ibe <Prosperibe12@gmail.com>
2025-03-09 23:58:33 +0000

    added kubernetes config

--- server.py
+++ server.py
@@ -92,6 +92,6 @@ def download():
     return {"error": "Not authorized"}, 401
 
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=5000)
+    server.run(host="0.0.0.0", port=8080)
 
     
